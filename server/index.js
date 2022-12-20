2da5dd72cfbb7cf0c3829e2a30f5b07ea645b1d6
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-20 16:53:05 +0300

    addition

--- server/index.js
+++ server/index.js
@@ -10,6 +10,6 @@ app.post("/", function (request, response){
     var n1 = Number(request.body.n1);
     var n2 = Number(request.body.n2);
     var result = n1 + n2;
-    response.send("The result of your calculation are: " + result);
+    response.send("The result of your addition calculation are: " + result);
 });
-app.listen(8000);
+app.listen(8080);
